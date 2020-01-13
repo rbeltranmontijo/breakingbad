0d6844e1e5b73e5924f55e37ce60453e6515b713
Robin Beltran <robinbeltran@MacBook-Pro-de-Robin.local>
2020-01-12 21:00:39 -0600

    terminando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Frase(props) {
+  return (
+    <div className="frase">
+      <h1>{props.frase.quote}</h1>
+      <p>--{props.frase.author}</p>
+    </div>
+  );
+}
 
 function App() {
+  const [frase, obtenerFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const resultado = await axios(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    console.log(resultado.data[0]);
+    // Agregar el sesultado dela API al state
+    obtenerFrase(resultado.data[0]);
+  };
+
+  // consilta a una rest api
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <Frase frase={frase} />
+      <button onClick={consultarAPI}>Generar Nueva</button>
     </div>
   );
 }
